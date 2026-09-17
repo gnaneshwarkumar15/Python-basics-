e9ed15ec46fdd204e13a975ae6e634d05b1b07b6
gnaneshwarkumar15 <gnaneshwarkumar15@gmail.com>
2026-09-17 16:53:08 +0530

    Add files via upload

---
+++ membership operators.py
@@ -0,0 +1,9 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=1,2,3,4,5,6,7,8
+>>> 8 in a
+True
+>>> 20 in a
+False
+>>> 20 not in a
+True
