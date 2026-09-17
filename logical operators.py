1bb1bfd2aadcb186aa18dfcea4b6ae895b1edc0b
gnaneshwarkumar15 <gnaneshwarkumar15@gmail.com>
2026-09-17 16:52:13 +0530

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,18 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=5
+>>> b=7
+>>> a<b and b>a
+True
+>>> a!=b and b>=a
+True
+>>> a<b or b>a
+True
+>>> a!=b and a==b
+False
+>>> a!=b or a==b
+True
+>>> not True
+False
+>>> not False
+True
