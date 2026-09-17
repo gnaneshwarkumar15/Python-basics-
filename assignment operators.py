ffadd77f52f093896b7189ed96c5fedb8fe5ed0e
gnaneshwarkumar15 <gnaneshwarkumar15@gmail.com>
2026-09-17 16:50:31 +0530

    Add files via upload

---
+++ assignment operators.py
@@ -0,0 +1,24 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=3
+>>> b=5
+>>> a+b
+8
+>>> a+=b
+>>> a
+8
+>>> b+=a
+>>> b
+13
+>>> a-=2
+>>> a
+6
+>>> b-=a
+>>> b
+7
+>>> a//=2
+>>> a
+3
+>>> a%=6
+>>> a
+3
