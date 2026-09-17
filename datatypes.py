57c83b3edf2cdea183f09b3c14ad24110ed64803
gnaneshwarkumar15 <gnaneshwarkumar15@gmail.com>
2026-09-17 16:47:07 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=10
+>>> type(a)
+<class 'int'>
+>>> b=3.4
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> d=5+7j
+>>> type(d)
+<class 'complex'>
+>>> e=True
+>>> type(e)
+<class 'bool'>
+>>> f=true
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    f=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
