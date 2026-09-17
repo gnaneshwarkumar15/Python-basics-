e2c5b75c116fa3a2d2f1389d961929adcd14644e
gnaneshwarkumar15 <gnaneshwarkumar15@gmail.com>
2026-09-17 16:51:14 +0530

    Add files via upload

---
+++ comparision operators.py
@@ -0,0 +1,26 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=4
+>>> b=7
+>>> a<b
+True
+>>> a>b
+False
+>>> a!=b
+True
+>>> a==b
+False
+>>> a<=b
+True
+>>> a=6
+>>> b=6
+>>> a>b
+False
+>>> a>b
+False
+>>> a==b
+True
+>>> a>=b
+True
+>>> a!=b
+False
