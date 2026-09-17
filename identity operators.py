6988a77cca6b62d5df11f3956e18fe12bb43d671
gnaneshwarkumar15 <gnaneshwarkumar15@gmail.com>
2026-09-17 16:51:47 +0530

    Add files via upload

---
+++ identity operators.py
@@ -0,0 +1,14 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=4
+>>> type(a) is int
+True
+>>> type(a) is not int
+False
+>>> a=5.6
+>>> type(a) is int
+False
+>>> type of (a) is not float
+SyntaxError: invalid syntax
+>>> type(a) is not float
+False
